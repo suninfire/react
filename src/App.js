bb71b0a71af1b2a3863f57a5e228a43fca1263aa
suninfire <n.suninfire@gmail.com>
2022-06-30 17:17:36 +0300

    hw_

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити нового юзера постовим запитом на http://jsonplaceholder.typicode.com/users
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити новий комментар постовим запитом на http://jsonplaceholder.typicode.com/comments
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useForm} from "react-hook-form";
+
+export default function App() {
+
+   let {register,handleSubmit,formState:{errors}} = useForm({defaultValues:{name:'name',username:'username'}});
+
+    const onSubmit = (data) => {
+
+        fetch('http://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
+
+    return (
+        <div>
+<h4>Create User</h4>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name",{required:true})}/>
+                {errors.name && <span>write yor name</span>}
+                <input {...register("username")}/>
+                <input type="submit" value={'save post'}/>
+                {/*<select {...register("userId")}>*/}
+                {/*    <option value="1">1</option>*/}
+                {/*    <option value="2">2</option>*/}
+                {/*    <option value="3">3</option>*/}
+                {/*    <option value="4">4</option>*/}
+                {/*    <option value="5">5</option>*/}
+                {/*</select>*/}
+            </form>
+
+        </div>
+    );
 }
-
-export default App;
---
+++ src/components/NewUserForm.js
@@ -0,0 +1,7 @@
+export default function NewUserForm() {
+    return (
+        <div>
+            NewUserForm
+        </div>
+    );
+}
