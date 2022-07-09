e2c97fc53cacd991c1da15e8e3732ac10f83cf5b
suninfire <n.suninfire@gmail.com>
2022-07-10 00:34:02 +0300

    hw 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// реалізувати 3 маршрути
+// todos - при переході на який тягнуться всі todo з https://jsonplaceholder.typicode.com/todos
+//
+// albums - при переході на який тягнуться всі альбоми з https://jsonplaceholder.typicode.com/albums
+//
+// comments - при переході на який тягнуться всі комментарі https://jsonplaceholder.typicode.com/comments
+// при натисканні на комментар тягнеться пост, до якого належіить цей коментар всі його пости. приклад запиту https://jsonplaceholder.typicode.com/posts/ID
+// id поста взяти з коментаря (postId)
+//
+// відображати ті чи інші маршрути можна на будь-якому рівні на ваш вибір.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {Link, Route, Routes} from "react-router-dom";
+import './App.css'
 
-export default App;
+import TodosPage from "./pages/TodosPage";
+import AlbumsPage from "./pages/AlbumsPage";
+import CommentsPage from "./pages/CommentsPage";
+
+export default function App() {
+    return (
+        <div className={'maindiv'}>
+
+            <div><Link to={'/'}> Clear Page </Link></div>
+
+          <div className={'menu'}>
+              <div><Link to={'/todos'}> Todos </Link></div>
+              <div><Link to={'/albums'}> Albums </Link></div>
+              <div><Link to={'/comments'}> Comments </Link></div>
+          </div>
+
+          <div>
+            <hr/>
+            <Routes>
+              <Route path={'/todos'} element={<TodosPage/>}/>
+              <Route path={'/albums'} element={<AlbumsPage/>}/>
+              <Route path={'/comments'} element={<CommentsPage/>}/>
+            </Routes>
+          </div>
+
+        </div>);}
+
